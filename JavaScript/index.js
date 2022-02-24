3d62a273d42d22770260c06731616e924cfd4c88
sharan <dropmailtosharan>
2022-02-24 21:27:31 +0530

    new changes

---
+++ JavaScript/index.js
@@ -0,0 +1,17 @@
+console.log("Hello World...!");
+console.log(2+25);
+console.log('Hello World...!');
+var x=12345;
+console.log(x);
+let name1='Sharan';
+// let if='Sharan';  It should be meaningful
+console.log(name1);  //cannot be a reserved keyword
+// Cannot start with a number (1name) we can start after the name (name1)
+// cannot contain a space or hyphen (-)
+
+let firstName='Sharan';lastName='Reddy'; //camel notation Are case-sensitive
+console.log(firstName+' '+lastName);
+let data=12345;
+console.log(data); console.log(typeof data);
+ a1="sharan"; console.log(a1);
+
